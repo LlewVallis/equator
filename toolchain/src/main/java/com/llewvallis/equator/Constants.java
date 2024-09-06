787c79afd65876702bf23cd65280152d7cf398db
LlewVallis <llewvallis@gmail.com>
2024-09-06 20:46:52 +1000

    Foo

--- toolchain/src/main/java/com/llewvallis/equator/Constants.java
+++ toolchain/src/main/java/com/llewvallis/equator/Constants.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Constants {
 
+    // Fixed
     public static final ObjectMapper JSON = new ObjectMapper();
 
     public static final String EQUATOR_DIRECTORY = ".equator";
